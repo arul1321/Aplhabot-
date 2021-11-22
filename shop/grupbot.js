90b711d9cf4a8dc07cebec39cc5ef5e42f509b85
Syahrul_Rahmadan (Arul_Ganz) <88493207+arulganz@users.noreply.github.com>
2021-11-22 16:52:29 +0700

    Add files via upload

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -1,18 +1,11 @@
 // jangan di ubah jika tidak punya group bot, takutnya eror
 const gcbotwa = () =>{
-	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+	return`
+*GAK PUNYA GC MASUKIN GC KALIAN DONG* 
+*TAPI JANGAN DI JADIIN ADMIN*
+*KALAU JADI ADMIN NTAR GW KELUARIN :)*
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
-
-Jika ada link yang ke reset, silahkan hubungi
-owner untuk meminta link yang baru
+Jangan lupa Follow Instagram ::::::::) *https://instagram.com/_daaa_1*
 `
 }
 exports.gcbotwa = gcbotwa

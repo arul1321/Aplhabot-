d433b9547c9720a6caaccdf9b2ff2a40af050bf9
Syahrul_Rahmadan (Arul_Ganz) <88493207+arulganz@users.noreply.github.com>
2021-11-22 17:03:45 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,33 +1,9 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
-
-*📲 PULSA*
-
-Tidak tersedia
-	
-Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+*NGAPAIN SIH PENCET PENCET SAMPE SINI*
+*KALAU ADA DONASI JUGA, GW YAKIN LU GAK BAKAL DONASI*
+*IYA KAN ? WKWKWKWKWKWK*
 `
 	}
 
